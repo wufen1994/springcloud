c9e18e84bbb436cf63440f80bbc15aa30a38ef33
wufen1994 <18102125205@163.com>
2021-11-05 10:05:11 +0800

    配置单机Eureka服务端及客户端

---
+++ cloud-eureka-server7001/src/main/java/com/it/springcloud/EurekaMain7001.java
@@ -0,0 +1,19 @@
+package com.it.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @program: springcloud
+ * @description: eureka的server启动类
+ * @author: wf
+ * @create: 2021-11-05 07:23
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
